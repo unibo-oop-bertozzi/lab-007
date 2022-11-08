3ff6b23f3c32a11ab87084dc8199d278681119d1
Niccolo Bertozzi <niccolo.bertozzi2@studio.unibo.it>
2022-11-08 13:11:11 +0100

    lab007

---
+++ 63-mvc-gui-reflection/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    application
+    java
+}
+
+application {
+    mainClass.set("it.unibo.mvc.LaunchApp")
+}
+
+tasks.javadoc {
+    isFailOnError = false
+}
+
+tasks.javadoc {
+    isFailOnError = false
+}
